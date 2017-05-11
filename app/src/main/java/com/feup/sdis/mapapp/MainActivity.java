407d90e99e976c3f02012f8e578e316d24547849
tirafesi <tirafesi@hotmail.com>
2017-05-11 17:42:39 +0100

    added googlemaps

--- app/src/main/java/com/feup/sdis/mapapp/MainActivity.java
+++ app/src/main/java/com/feup/sdis/mapapp/MainActivity.java
@@ -1,13 +1,49 @@
 package com.feup.sdis.mapapp;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnStart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnStart = (Button) findViewById(R.id.btn_start);
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (isConnected()) {
+                    Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(MainActivity.this, "No Internet Connection", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+
+    /**
+     * Checks whether network connectivity exists and it is possible to establish connections and pass data.
+     *
+     * @return true if network connectivity exists, false otherwise.
+     */
+    public boolean isConnected() {
+
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+
+        return (activeNetwork != null && activeNetwork.isConnected());
     }
 }
